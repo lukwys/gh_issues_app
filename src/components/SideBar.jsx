ef1258fa35dfd7972a7db5f8160789b9c17d52ce
lwyso <lukasz-wysocki18@wp.pl>
2020-12-23 23:49:15 +0100

    adjust layout

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 import { Navigation } from './Navigation';
+import menu_icon from '../assets/menu.png';
+import cross_icon from '../assets/cross-mark.png';
 import './side-bar.scss'
 
 export const SideBar = () => {
   const [isMenuActive, setMenuActive] = useState(false);
+  const [activeIcon, setActiveIcon] = useState(menu_icon);
 
   const onHamburgerClick = () => {
+    setActiveIcon(!isMenuActive ? cross_icon : menu_icon);
     setMenuActive(!isMenuActive);
   }
 
   return (
     <section className={`side-bar ${isMenuActive ? 'side-bar--active' : ''}`}>
-      <div className="side-bar__hamburger" onClick={onHamburgerClick}></div>
+      <div className="side-bar__hamburger-wrapper" onClick={onHamburgerClick}>
+        <img className="side-bar__hamburger-icon" src={activeIcon} alt="menu icon"/>
+        <p className={activeIcon}></p>
+      </div>
       <Navigation />
     </section>
   )
